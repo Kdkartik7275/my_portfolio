4d424351ec36343aa351425bc86e13a1b7e8ec4a
KdKartik7275 <kartik.kd8954@gmail.com>
2024-05-23 15:30:01 +0530

    changes made

--- lib/utils/sns_links.dart
+++ lib/utils/sns_links.dart
@@ -1,5 +1,4 @@
 class SNSLinks {
-  static String linkedIn = "https://linkedin.com/in/kartik-dhiman-374725155";
-  static String upWork =
-      "https://www.upwork.com/freelancers/~01ccf027fe67b2e31a";
+  static String linkedIn = "";
+  static String upWork = "";
 }
